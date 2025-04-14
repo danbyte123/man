114ab1b301b72bb60630e31190241a9fd85cd90d
danbyte123 <mokj2019@gmail.com>
2025-04-14 17:38:51 +0300

    new

--- sortedarray.cpp
+++ sortedarray.cpp
@@ -36,13 +36,78 @@ int getlength (){
 int makeempty (){
     length = 0 ;}
 
+    void retrieveItem(int arr[], int length, int& element, bool& found) {
+        found = false;
+        int location = 0;
+        
+        while (location < length && !found) {
+            if (arr[location] == element) {
+                found = true; // we found it
+            } else if (element < arr[location]) {
+                // since the array is sorted, no need to keep looking
+                break;
+            } else {
+                location++;
+            }
+        }
+    }
+    
+void insertitem (int elment ){
+    int location = 0 ;
+    bool found = false ;
+    if (isfull ()){
+        cout << " array is full " << endl ;
+    }
+    else {
+        while ( (location < length ) && !found  ){
+            if (arr[location] == elment){
+                cout << " element already exists " << endl ;
+                found = true ;
+            }
+            else if (arr[location] > elment){
+                found = true ;
+            }
+            else {
+                location++;
+            }
+        }
 
+    }
+    for ( int index = length ; index > location ; index--){
+        arr[index] = arr[index - 1];
+    }
+    arr[location] = elment ;
+    length++;
+}
+void deleteitem ( int elment ){
+    int location = 0;
+    bool found = false ;
+    while ( elment != arr[location]){
+        location++;
+    }
+    for ( int index = location+1; index < length -1 ; index++){
+        arr[index -1 ] = arr[index];
+    }
+    length--;
+}
+void resetlist (){
+    currpos = -1 ;
+}
+bool islastitem () const {
+    return (currpos == length -1);
+
+}
+bool getnextitem (int& element){
+    if (currpos == length -1){
+        cout << " no more items " << endl ;
+        return false ;
+    }
+    else {
+        currpos++;
+        element = arr[currpos];
+        return true ;
+    }
+}
 };
-
-
-
-
-
-
 
 int main (){}
